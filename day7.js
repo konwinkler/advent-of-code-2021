c0e70bd6b63ef1e6809e62bf97bb2483a8e6d568
Konrad Winkler <konrad.winkler@willowtreeapps.com>
2021-12-07 08:54:20 -0500

    day 7 part 2

--- day7.js
+++ day7.js
@@ -9,21 +9,39 @@ const readFile = (fileName) => {
 const example = `
 16,1,2,0,4,2,7,1,2,14
 `
-
-const calculateFuelCost = (positions, target) => {
-    let cost = 0
-    for (const position of positions) {
-        cost += Math.abs(target - position)
+const cache = new Map()
+const fuelIncreasingCost = (steps) => {
+    if (steps === 0) {
+        return 0
+    } else {
+        if (cache.has(steps)) {
+            return cache.get(steps)
+        }
+        const cost = fuelIncreasingCost(steps - 1) + steps
+        cache.set(steps, cost)
+        return cost
     }
-    return cost
 }
 
-const alignCrabs = (input) => {
+const calculateFuelCost = (positions, target, simple) => {
+    let totalCost = 0
+    for (const position of positions) {
+        const steps = Math.abs(target - position)
+        if (simple) {
+            totalCost += steps
+        } else {
+            totalCost += fuelIncreasingCost(steps)
+        }
+    }
+    return totalCost
+}
+
+const alignCrabs = (input, simple = true) => {
     const startingPositions = input.trim().split(',').map(Number)
     const rightMostCrab = Math.max(...startingPositions)
     let lowestFuelCost = Number.MAX_SAFE_INTEGER
     for (let i = 0; i <= rightMostCrab; i++) {
-        const fuelCost = calculateFuelCost(startingPositions, i)
+        const fuelCost = calculateFuelCost(startingPositions, i, simple)
         lowestFuelCost = Math.min(fuelCost, lowestFuelCost)
     }
     return lowestFuelCost
@@ -31,4 +49,5 @@ const alignCrabs = (input) => {
 
 assert.equal(alignCrabs(example), 37)
 console.log('part 1', alignCrabs(readFile('input7.txt')))
-
+assert.equal(alignCrabs(example, false), 168)
+console.log('part 2', alignCrabs(readFile('input7.txt'), false))
