060b56b9fcf904cc7bd8494cdecf7a41c08317ef
Konrad Winkler <konrad.winkler@willowtreeapps.com>
2021-12-09 21:28:02 -0500

    day 9 part 2

--- day9.js
+++ day9.js
@@ -40,3 +40,77 @@ const sumRiskLevels = (input) => {
 
 assert.equal(sumRiskLevels(example), 15)
 console.log('part 1', sumRiskLevels(data))
+
+const bfs = (inputX, inputY, map) => {
+    const width = map[0].length
+    const height = map.length
+
+    const queue = []
+    let basinSize = 0
+    queue.push({x: inputX, y: inputY})
+    while (queue.length > 0) {
+        const {x, y} = queue.shift()
+        // check if valid
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            const current = map[y][x]
+            if (current === '.') {
+                basinSize++
+                map[y][x] = '*'
+                // add surrounding tiles
+                queue.push({x: x + 1, y})
+                queue.push({x: x - 1, y})
+                queue.push({x, y: y - 1})
+                queue.push({x, y: y + 1})
+            } else {
+                // ignore since not empty
+            }
+        } else {
+            // ignore since invalid
+        }
+    }
+    return basinSize
+}
+
+const MultiplyThreeLargestBasins = (input) => {
+    /**
+     * 9s are walls, lowers become empty spot .
+     * BFS at empty spot to find size -> mark them
+     * repeat until all spots are walls or marked
+     */
+     const lines = input.trim().split('\n')
+     const map = []
+     for (const line of lines) {
+         const row = line
+            .split('')
+            .map(n => {
+                if (n === '9') {
+                    return '#'
+                } else {
+                    return '.'
+                }
+            })
+         map.push(row)
+     }
+    //  console.table(map)
+
+     const width = map[0].length
+     const height = map.length
+
+     const basinSizes = []
+     for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const current = map[y][x]
+            if (current === '.') {
+                const basinSize = bfs(x, y, map)
+                basinSizes.push(basinSize)
+                // console.table(map)
+            }
+        }
+    }
+    basinSizes.sort((a, b) => b - a)
+    // console.log(basinSizes)
+    return basinSizes[0] * basinSizes[1] * basinSizes[2]
+}
+
+assert.equal(MultiplyThreeLargestBasins(example), 1134)
+console.log('part 2', MultiplyThreeLargestBasins(data))
