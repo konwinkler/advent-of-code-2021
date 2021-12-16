cda97a71fa0a8ad79d524d2db23932876f4cb5d7
Konrad Winkler <konrad.winkler@willowtreeapps.com>
2021-12-15 21:18:19 -0500

    day 15 part 2, too 2 minutes to calculate :(

--- day15/index.js
+++ day15/index.js
@@ -9,35 +9,49 @@ const day = 15
 const example = readFile(`day${day}/example`)
 const data = readFile(`day${day}/input.txt`)
 
-const createNeighbors = (current, map, width, height) => {
+const wrapCost = (i) => {
+    return (i - 1) % 9 + 1
+}
+
+const getCost = (map, x, y, width, height, multiply) => {
+    if (multiply === 1) {
+        return map[y][x]
+    }
+    const realX = x % width
+    const realY = y % height
+    const offset = Math.floor(x / width) + Math.floor(y / height)
+    return wrapCost(map[realY][realX] + offset)
+}
+
+const createNeighbors = (current, map, width, height, multiply) => {
     const neighbors = []
     const { x, y, cost } = current
     if (x > 0) {
         neighbors.push({
             x: x - 1,
             y: y,
-            cost: cost + map[y][x - 1]
+            cost: cost + getCost(map, x - 1, y, width, height, multiply)
         })
     }
     if (y > 0) {
         neighbors.push({
             x: x,
             y: y - 1,
-            cost: cost + map[y - 1][x]
+            cost: cost + getCost(map, x, y - 1, width, height, multiply)
         })
     }
-    if (x < width - 1) {
+    if (x < width * multiply - 1) {
         neighbors.push({
             x: x + 1,
             y: y,
-            cost: cost + map[y][x + 1]
+            cost: cost + getCost(map, x + 1, y, width, height, multiply)
         })
     }
-    if (y < height - 1) {
+    if (y < height * multiply - 1) {
         neighbors.push({
             x: x,
             y: y + 1,
-            cost: cost + map[y + 1][x]
+            cost: cost + getCost(map, x, y + 1, width, height, multiply)
         })
     }
     return neighbors
@@ -47,7 +61,7 @@ const getNodeFromList = (neighbor, visited) => {
     return visited.find(node => node.x === neighbor.x && node.y === neighbor.y)
 }
 
-const lowestRiskPath = (input) => {
+const lowestRiskPath = (input, multiply = 1) => {
     const lines = input.trim().split('\n')
     const map = lines.map(line => line.split('').map(Number))
 
@@ -63,12 +77,12 @@ const lowestRiskPath = (input) => {
     // take element with lowest cost
     while (current !== undefined) {
         // if goal then we are done
-        if (current.x === width - 1 && current.y === height - 1) {
+        if (current.x === width * multiply - 1 && current.y === height * multiply - 1) {
             return current.cost
         }
 
         // create neighbors
-        const neighbors = createNeighbors(current, map, width, height)
+        const neighbors = createNeighbors(current, map, width, height, multiply)
         for (const neighbor of neighbors) {
 
             if (getNodeFromList(neighbor, visited) === undefined) {
@@ -85,8 +99,15 @@ const lowestRiskPath = (input) => {
 
         visited.push(current)
         current = nextQueue.shift()
+        if (visited.length % 10000 === 0) {
+            console.log(`visited ${visited.length} nodes`)
+        }
     }
 }
 
 assert.equal(lowestRiskPath(example), 40)
 console.log('part 1', lowestRiskPath(data))
+
+assert.equal(lowestRiskPath(example, 5), 315)
+console.log('part 2', lowestRiskPath(data, 5))
+
